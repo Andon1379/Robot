c6cbfced61ac11ee415ae9f1c1ebf83554b0234d
Andon1379 <afcb1379@gmail.com>
2022-01-05 14:56:35 -0500

    hehehe

---
+++ include/robot-config.h
@@ -0,0 +1,20 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern controller Controller1;
+extern motor FrontLeft;
+extern motor BackLeft;
+extern motor FrontRight;
+extern motor BackRight;
+extern motor LiftMotor;
+extern motor ClawMotor;
+extern motor LiftMotor2;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
